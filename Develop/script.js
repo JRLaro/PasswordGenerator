44a99f04a8931f29b1be8ffe434ebe242ca4a141
J.Laro <joselarojr@gmail.com>
2020-12-18 09:24:28 -0500

    refactored - added pseudocoding/comments on JS file. Breakdown the steps and what is being done and how it is being done

--- Develop/script.js
+++ Develop/script.js
@@ -1,122 +1,144 @@
-//Prompting the user to select desired length of Password between 8-128
- 
-var userInput = prompt("Select desired length of Password between 8-28 characters");
+// List of User Inputs Variables
+var userInput;
+var useNumber;
+var useSpecialCharacter;
+var useUpperCase;
+var useLowerCase;
+var choices = [];
 
-// if user selects number less than 8 - alert (Selection must be within 8-28 characters)
+// Array of special characters to be included in password
+var specialCharacters = ["@", "%", "+", "/", "”", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."]
+// Array of numeric characters to be included in password
+var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-// if (userInput >= 8) {
-//   userChoice();
-  
-// }
-//   else {
-//     prompt("Selection must be within 8-28 characters.");
-// }
-
-if(userInput <8 || userInput > 28){
-  alert("It is recommended to have a password between 8 and 28 characters.");
-}
- else {
-   userChoice();
- }
+/**
+ * function getUserInput
+ *  - check if number
+ *  - if not run getUserInput again
+ *  - if number move on
+ */
 
 
- else{
-  if(confirm("Would you like your password to contain upper case letters?"));
-
+function getUserInput() {
+  userInput = prompt("How many characters would you like your password to be? Password must be at least 8 characters and no more than 128.");
+  // If user input is not a number
+  if (isNaN(userInput)) {
+    alert("Please enter a valid number.");
+    getUserInput();
   }
-
-  if(confirm("Would you like your password to contain lower case letters?"));
+  // If invalid number is selected “please input a number between 8 and 128.”
+  else if (userInput < 8 || userInput > 128) {
+    userInput = prompt("Please input a number between 8 and 128.");
+    getUserInput();
   }
-
-  if(confirm("Would you like your password to contain numbers?"));
+  // Ask user for special, number, lower, and uppercase characters
+  else {
+    userChoice()
   }
+};
 
-  if(confirm("Would you like your password to contain symbols?"));
-  }
-
-  if(allChars.length===0){
-      alert("You must select at lease 1 type of characters to generate a password!\nPlease start over.");
-  }
-
-
-
-
-
-
-//  while (userInput) {
-//   prompt("Select desired length of Password between 8-28 characters");
-// }
-
-
-// Write a function that prompts user if the want specialCharacter, number, lowerCase, upperCase
-
+// Create a function that ask user for special, number, lower, and uppercase characters.
 function userChoice() {
-if (userSpecial = confirm("Would you like to use Special Characters?");
-
-}
-if (userNumber = confirm("Would you like to use Numbers?");
-if (userUpper = confirm("Would you like to use Upper Case Letters?");
-if (userLower = confirm("Would you like to user Lower Case Letters?");
-
-
+  useSpecialCharacter = confirm("Would you like to use special characters in your password?");
+  useNumber = confirm("Would you like to use numbers in your password");
+  useLowerCase = confirm("Would you like to use lowercase characters in you password?");
+  useUpperCase = confirm("Would you like to use uppercase characters in you password?");
 }
 
+// Create a function called generatePassword for var password
+function generatePassword() {
 
+  // Gets button to ask user for userChoice.
+  getUserInput();
 
-// if user selects number that is between 8-28 then... prompted to select if special character
+  // If user declines special, number, lower, and uppercase characters “you must select one criteria”
+  if (!useSpecialCharacter && !useNumber && !useUpperCase && !useLowerCase) {
+    alert("You must select one criteria.");
+    userChoice();
+  }
 
+  // First if statement that uses user input prompts to determine choices
 
-//
+  // Else if for 4 positive options
 
+  else if (useSpecialCharacter && useNumber && useLowerCase && useUpperCase) {
+    choices = specialCharacters.concat(numberCharacters, lowerCasedCharacters, upperCasedCharacters);
+  }
+  // Else if for 3 positive options
 
+  else if (useSpecialCharacter && useNumber && useUpperCase) {
+    choices = specialCharacters.concat(numberCharacters, upperCasedCharacters);
+  }
+  else if (useSpecialCharacter && useNumber && useLowerCase) {
+    choices = specialCharacters.concat(numberCharacters, lowerCasedCharacters);
+  }
+  else if (useSpecialCharacter && useLowerCase && useUpperCase) {
+    choices = specialCharacters.concat(lowerCasedCharacters, upperCasedCharacters);
+  }
+  else if (useNumber && useLowerCase && useUpperCase) {
+    choices = numberCharacters.concat(lowerCasedCharacters, upperCasedCharacters);
+  }
 
+  // Else if for 2 positive options
+  else if (useSpecialCharacter && useNumber) {
+    choices = specialCharacters.concat(numberCharacters);
+  }
+  else if (useSpecialCharacter && useLowerCase) {
+    choices = specialCharacters.concat(lowerCasedCharacters);
+  }
+  else if (useSpecialCharacter && useUpperCase) {
+    choices = specialCharacters.concat(upperCasedCharacters);
+  }
+  else if (useLowerCase && useNumber) {
+    choices = lowerCasedCharacters.concat(numberCharacters);
+  }
+  else if (useLowerCase && useUpperCase) {
+    choices = lowerCasedCharacters.concat(upperCasedCharacters);
+  }
+  else if (useNumber && useUpperCase) {
+    choices = numberCharacters.concat(upperCasedCharacters);
+  }
 
+  // Else if for 1 positive option
+  else if (useSpecialCharacter) {
+    choices = specialCharacters;
+  }
+  else if (useNumber) {
+    choices = numberCharacters;
+  }
+  else if (useLowerCase) {
+    choices = lowerCasedCharacters;
+  }
+  else if (useUpperCase) {
+    choices = upperCasedCharacters;
+  }
 
+  // Start random selection variables:
+  // Random selection for all variables:
 
+  var finalPassword = "";
+  for (var i = 0; i < userInput; i++) {
+    var character = choices[Math.floor(Math.random() * choices.length)];
+    finalPassword += character;
+  }
+  return finalPassword;
+};
 
+// This selects the Button In HTML
 
-
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// When button is clicked - run (function) "writePassword"
+
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// Create Variables
-Character = [""]
-var userSpecial = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
-];
-// Array of numeric characters to be included in password
-var userNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-// Array of lowercase characters to be included in password
-var userLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-];
-// Array of uppercase characters to be included in password
-var userUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-];
-
-
-// Prompt the User to begin
- 
-
-
-// The function
-
-
-
-
-
-
-
-/
+};
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # PasswordGenerator
+
+
+## Description 
+
+> In this Project I had to create a randomized generated Password based off criteria 
+
+
+See for yourself - Check it out: 
+https://jrlaro.github.io/HomeWork_02/index.html
+
+
+## Installation
+
+Being that this is not an application but rather just a wesbite containing my personal Portfolio, the only things that are required is access to the internet as well as any web-browser (whether that be Google Chrome, Safari, Mozilla-Firefox, Internet-Explorer, Microsoft-Edge...etc.)
+ 
+
+
+## Usage 
+
+
+## Credits
+
+No collaborators were harmed in the making of this website.
+
+---
